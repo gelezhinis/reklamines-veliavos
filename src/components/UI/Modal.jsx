6252fac3660395a3c51d3af522cb7d2079b339e1
Linas Geležauskas <gelezhinis@gmail.com>
2023-06-30 18:28:16 +0300

    fixed modal position

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import classes from './Modal.module.css';
 
 const Modal = ({component}) => {
+  useEffect(() => {
+    window.scroll(0, 0);
+  },[]);
+
   return (
     <div className={classes.modal}>{component}</div>
   );
