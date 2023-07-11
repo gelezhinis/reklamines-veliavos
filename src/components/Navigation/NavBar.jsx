42ae6bc6f8e9eb2b9b9348695a2996ac6ed5da03
Linas Geležauskas <gelezhinis@gmail.com>
2023-07-11 15:00:41 +0300

    nieko nesupratau, neveikia bl

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,34 +1,60 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-import LazyLoad from 'react-lazy-load';
+import { NavLink } from 'react-router-dom';
+// import LazyLoad from 'react-lazy-load';
 
-import {FaFacebook, FaInstagram} from 'react-icons/fa';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 
-import logo from  '../../img/logo/logo_n.png';
+import logo from '../../img/logo/logo_n.png';
 import classes from './NavBar.module.css';
 
 const NavBar = () => {
   return (
     <div className={classes.nav}>
       <div className={classes.nav_wrap}>
-        <div><NavLink to="/"><LazyLoad offset={50}><img src={logo} width={220} alt="logo" /></LazyLoad></NavLink></div>
+        <div>
+          <NavLink to="/">
+            {/* <LazyLoad offset={50}> */}
+              <img src={logo} width={220} alt="logo" />
+            {/* </LazyLoad> */}
+          </NavLink>
+        </div>
         <ul className={classes.nav_list}>
           <li>
-            <NavLink to="/price" activeClassName={classes.active}>Kainos</NavLink>
+            <NavLink to="/price" activeClassName={classes.active}>
+              Kainos
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/gallery" activeClassName={classes.active}>Galerija</NavLink>
+            <NavLink to="/gallery" activeClassName={classes.active}>
+              Galerija
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" activeClassName={classes.active}>Kontaktai</NavLink>
+            <NavLink to="/contact" activeClassName={classes.active}>
+              Kontaktai
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/order" activeClassName={classes.active}>Užsakyti</NavLink>
+            <NavLink to="/order" activeClassName={classes.active}>
+              Užsakyti
+            </NavLink>
           </li>
         </ul>
         <div className={classes.social_list}>
-          <a href="https://www.facebook.com/reklaminesveliavos" target="_blank" rel="noopener noreferrer"><FaFacebook size={25} /></a>
-          <a href="https://www.instagram.com/myprint.lt" target="_blank" rel="noopener noreferrer"><FaInstagram size={25} /></a>
+          <a
+            href="https://www.facebook.com/reklaminesveliavos"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook size={25} />
+          </a>
+          <a
+            href="https://www.instagram.com/myprint.lt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={25} />
+          </a>
         </div>
       </div>
     </div>
